c92cfea3a84c5c75b1591df394cbd2f97d69fb7b
Samiran <samiran.gode.work@gmail.com>
2024-10-25 09:47:10 +0200

    half done

---
+++ 1800/Rendezvous_de_Marian_et_Robin.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void work(){
+    int n, m, h;
+
+    cin>>n;
+    cin>>m;
+    cin>>h;
+
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    int t;
+    cin>>t;
+    while(t--){
+        work();
+    }
+}
